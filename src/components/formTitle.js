e947e0bd3121a7d9f111b74454384d9fe6c0ed27
mechamlogan <mechamlogan@gmail.com>
2018-07-31 10:50:52 -0600

    commiting just to be safe because unsaved files

--- src/components/formTitle.js
+++ src/components/formTitle.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export function FormTitle(props) {
-    const { className, text } = props;
+    const { className, text } = props; 
     return (
         <h1 className={`${className} form-title`}>{text}</h1>
     )
