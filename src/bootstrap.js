792257aa22b5c10a0203c2e6ed91e926b3564612
mechamlogan <mechamlogan@gmail.com>
2018-07-30 22:07:21 -0600

    refactored quite a bit

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -8,23 +8,24 @@ import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
+
+import Layout from './components/layout';
+
 import Signup from './components/auth/signup';
 import Signin from './components/auth/signin';
-
-import HeaderWrapper from './components/headerWrapper';
-
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
         <Switch>
-          <HeaderWrapper>
+          <Layout>
             <Route path='/' exact component={Signin}/>
+            <Route path='/signin' component={Signin}/>
             <Route path='/signup' component={Signup}/>
-          </HeaderWrapper>
+          </Layout>
         </Switch>
       </BrowserRouter>
     </Provider>
