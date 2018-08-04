013388887f4ab2f81ec3b43bd51944305e97881f
mechamlogan <mechamlogan@gmail.com>
2018-08-03 21:04:49 -0600

    built the newsletter grid

---
+++ src/components/newsletter/newsletterGrid.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewsletterGrid extends Component {
+    render() {
+        return (
+            <div className="newsletter-grid">
+                testing testing
+            </div>
+        )
+    }
+}
+
+export default NewsletterGrid;
