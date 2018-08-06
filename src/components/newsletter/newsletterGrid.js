6dbbecf57b9d651fabce6be2e07d89f677ca0c28
mechamlogan <mechamlogan@gmail.com>
2018-08-06 09:03:19 -0600

    mapped over a newsletter JSON item to the latest newsletter component

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -6,12 +6,20 @@ import NewsletterLatest from './newsletterLatest';
 
 class NewsletterGrid extends Component {
     render() {
+
+        const latest =  {
+            _id: '114',
+            title: 'Happy Holidays',
+            body: 'Bacon ipsum dolor amet cow strip steak boudin hamburger chicken, rump tongue meatball turducken shoulder prosciutto pork belly sirloin tri-tip. Picanha sirloin jowl biltong corned beef meatball, buffalo pastrami strip steak ground round porchetta cupim. Cupim tenderloin chuck ground round. Drumstick pastrami prosciutto short ribs flank burgdoggen cow sausage frankfurter fatback bacon ham tongue biltong. Landjaeger short ribs brisket sausage. Landjaeger porchetta ground round buffalo spare ribs tongue. Pork loin venison pork turkey, ground round picanha turducken.',
+            date: new Date(),
+            imageUrl: 'http://via.placeholder.com/960x258'
+        }
         return (
             <div className="newsletter-grid">
                 {/* add button */}
                 <NewsletterBox date={new Date()}/>
                 <NewsletterArchive />
-                <NewsletterLatest />
+                <NewsletterLatest {...latest}/>
             </div>
         )
     }
--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -2,18 +2,15 @@ import React, { Component } from 'react';
 
 class NewsletterLatest extends Component {
     render() {
+
+        const { title, imageUrl, body } = this.props;
+
         return (
             <div className="newsletter-latest">
-                <h1 className="newsletter-latest__title">Title goes here</h1>
-                <img className="newsletter-latest__image" src='http://via.placeholder.com/960x258'/>
+                <h1 className="newsletter-latest__title">{title}</h1>
+                <img className="newsletter-latest__image" src={imageUrl}/>
                 <div className="newsletter-latest__body">
-                    <p>Bacon ipsum dolor amet cow strip steak boudin hamburger chicken, rump tongue meatball turducken shoulder prosciutto pork belly sirloin tri-tip. Picanha sirloin jowl biltong corned beef meatball, buffalo pastrami strip steak ground round porchetta cupim. Cupim tenderloin chuck ground round. Drumstick pastrami prosciutto short ribs flank burgdoggen cow sausage frankfurter fatback bacon ham tongue biltong. Landjaeger short ribs brisket sausage. Landjaeger porchetta ground round buffalo spare ribs tongue. Pork loin venison pork turkey, ground round picanha turducken.
-
-                        Pork loin short loin ham venison pork chop shoulder kielbasa. Meatloaf pork belly venison kevin, tongue leberkas turkey. Frankfurter bresaola biltong burgdoggen leberkas kevin hamburger. Tenderloin capicola porchetta cupim hamburger flank frankfurter meatloaf ball tip pork jerky kevin swine tri-tip kielbasa. Flank jerky hamburger ham hock. Capicola jowl boudin, turkey shankle brisket jerky ground round hamburger sausage bacon meatball flank rump. Capicola kevin bacon cow pork chop.
-
-                        Salami prosciutto venison tongue turkey hamburger brisket. Ham hock pork belly turkey tri-tip rump meatball hamburger chuck. Andouille turkey meatloaf ground round picanha landjaeger t-bone short ribs pork loin tri-tip tenderloin turducken. Sausage strip steak pastrami drumstick turkey, rump hamburger alcatra ball tip meatball shank jowl kielbasa. Leberkas alcatra pork chop biltong. Cupim strip steak prosciutto, doner chicken rump tri-tip kielbasa.
-
-                    </p>
+                    <p>{body}</p>
                 </div>
             </div>
         )
